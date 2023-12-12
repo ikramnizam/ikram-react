2f7da3a19b94fef77327e341716091d8b0cbb448
ikramnizam <ikrmnzm@gmail.com>
2023-12-12 15:45:47 +0800

    tutorial 12

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,15 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
-        {title: 'My new website', body:'lorem ipsum', author:'ikram', id:1},
+        {title: 'My new website', body:'lorem ipsum', author:'alip', id:1},
         {title: 'tesdrgdfdgdggegtt', body:'lorem ipsum', author:'ikram', id:2},
-        {title: 'sesegsrgrsgr', body:'lorem ipsum', author:'ikram', id:3}
+        {title: 'sesegsrgrsgr', body:'lorem ipsum', author:'kkkk', id:3}
     ]);
 
     return ( 
         <div className="home">
           <BlogList blogs={blogs} title="All blog" />
+          <BlogList blogs={blogs.filter((blog) => blog.author === 'ikram')} title="ikram blog" />
         </div>
      );
 }
